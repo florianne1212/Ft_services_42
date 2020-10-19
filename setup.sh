c234e941400c4831a2d72318f22ccb8fb0dccabe
user42 <fcoudert@student.42.fr>
2020-10-19 16:20:34 +0200

    telegraph and influxdb work but not nginx

--- setup.sh
+++ setup.sh
@@ -16,22 +16,31 @@ eval $(minikube docker-env)
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 # On first install only
-
-
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f srcs/metallb/metallb-config.yaml
 #kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 
 docker build -t alpine_nginx srcs/nginx
 docker build -t alpine_mysql srcs/mysql/
-docker build -t service_wordpress srcs/wordpress/
+docker build -t alpine_wordpress srcs/wordpress/
 docker build -t alpine_phpmyadmin srcs/phpmyadmin/
-docker build -t alpine_ftps srcs/ftps/
+#docker build -t alpine_ftps srcs/ftps/
+docker build -t alpine_influxdb srcs/influxdb/
+docker build -t alpine_telegraf srcs/telegraf/
+#docker build -t alpine_grafana srcs/grafana/
 
-kubectl apply -f srcs/metallb/metallb-config.yaml
+
+
+
+
 kubectl apply -f srcs/nginx/nginx-deployment.yaml
 kubectl apply -f srcs/mysql/mysql-deployment.yaml
 kubectl apply -f srcs/wordpress/wordpress-deployment.yaml
 kubectl apply -f srcs/phpmyadmin/php-deployment.yaml
-kubectl apply -f srcs/ftps/ftps-deployment.yaml
+#kubectl apply -f srcs/ftps/ftps-deployment.yaml
+kubectl apply -f srcs/telegraf/telegraf-deployment.yaml
+#kubectl apply -f srcs/grafana/grafana_deployment.yaml
+kubectl apply -f srcs/influxdb/influxdb_deployment.yaml
 
 kubectl get pods
