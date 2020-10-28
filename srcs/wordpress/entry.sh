6aac17f7cbfcc01cf53448280ae96f9b84b57660
user42 <fcoudert@student.42.fr>
2020-10-28 15:08:29 +0100

    wordpress et phpmyadmin marche

--- srcs/wordpress/entry.sh
+++ srcs/wordpress/entry.sh
@@ -1,21 +1,24 @@
 
+cd /var/www/;
+mkdir -p wordpress;
+cd wordpress;
 
 if ! $(wp core is-installed); then
-    cd /www/wordpress;
     wp core download;
-
-    rm -rf wp-config.php;
-
+    
     while true; do
-        if wp config create --dbname=wordpress --dbuser=newuser --dbpass=newuser --dbhost=mysql; then
+       if wp config create --dbname=wordpress --dbuser=newuser --dbpass=newuser --dbhost=mysql; then
             break;
         fi;
-        sleep 5
+
+        sleep 5;
     done;
 
-    wp core install --url=172.17.0.3: --title=ft_services --admin_user=florianne --admin_password=12STRONGpassword --admin_email=info@example.com || true;
+    #wp language core activate en_US
+    wp core install --url=http://172.17.0.3:5050 --title=ft_services --admin_user=florianne --admin_password=12STRONGpassword --admin_email=info@example.com;
+    #wp core install --url=172.17.0.3: --title=ft_services --admin_user=florianne --admin_password=12STRONGpassword --admin_email=info@example.com || true;
 
-    wp user create author author@example.com --role=author
-    wp user create contributor contributor@example.com --role=contributor
-    wp user create user1 user1@example.com
+    wp user create author author@example.com --role=author;
+    wp user create contributor contributor@example.com --role=contributor;
+    wp user create user1 user1@example.com;
 fi
