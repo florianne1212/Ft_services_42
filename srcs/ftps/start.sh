33c36bdd026b1569e14a7cad6a975d281e804172
fcoudert <fcoudert@student.fr>
2020-11-04 11:24:31 +0100

    ftps marche

--- srcs/ftps/start.sh
+++ srcs/ftps/start.sh
@@ -1,1 +1,8 @@
-exec /usr/sbin/vsftpd -opasv_min_port=2100 -opasv_max_port=21000 -opasv_adress=172.17.0.3 /etc/vsftpd/vsftpd.conf
+
+#!/bin/sh
+
+echo -e "$PASSWORD\n$PASSWORD" | adduser -h ftp/$USER -s /sbin/nologin $USER
+mkdir -p ftp/$USER
+chown $USER:$USER ftp/$USER
+
+exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21000 -opasv_address=192.168.49.3 /etc/vsftpd/vsftpd.conf
--- srcs/wordpress/entry.sh
+++ srcs/wordpress/entry.sh
@@ -15,8 +15,8 @@ if ! $(wp core is-installed); then
     done;
 
     #wp language core activate en_US
-    wp core install --url=http://172.17.0.3:5050 --title=ft_services --admin_user=florianne --admin_password=12STRONGpassword --admin_email=info@example.com;
-    #wp core install --url=172.17.0.3: --title=ft_services --admin_user=florianne --admin_password=12STRONGpassword --admin_email=info@example.com || true;
+    wp core install --url=http://192.168.49.3:5050 --title=ft_services --admin_user=florianne --admin_password=12STRONGpassword --admin_email=info@example.com;
+    #wp core install --url=192.168.49.3: --title=ft_services --admin_user=florianne --admin_password=12STRONGpassword --admin_email=info@example.com || true;
 
     wp user create author author@example.com --role=author;
     wp user create contributor contributor@example.com --role=contributor;
