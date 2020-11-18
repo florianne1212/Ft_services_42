f945f9410fcef828c3cf9368aca725f300b2b725
fcoudert <fcoudert@student.fr>
2020-11-18 14:38:55 +0100

    clean

--- srcs/mysql/init.sql
+++
@@ -1,8 +0,0 @@
-use mysql;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
-FLUSH PRIVILEGES;
-
-CREATE DATABASE IF NOT EXISTS `wordpress`;
-CREATE USER 'newuser'@'%' IDENTIFIED BY 'newuser' ;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'newuser'@'%';
-FLUSH PRIVILEGES;
